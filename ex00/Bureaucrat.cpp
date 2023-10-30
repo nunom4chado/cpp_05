42485f2b9accff23cbc503aa96241f76e708d04c
nunom4chado <nunom4chado@gmail.com>
2023-10-30 22:28:08 +0000

    adds ex00

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,5 +1,56 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(/* args */) {}
+Bureaucrat::Bureaucrat() { throw Bureaucrat::GradeTooLowException(); }
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+    : _name(name), _grade(grade) {
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+    : _name(src._name), _grade(src._grade) {}
 
 Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs) {
+    if (this != &rhs)
+        this->_grade = rhs._grade;
+
+    return *this;
+}
+
+/* --------------------------------- Getters -------------------------------- */
+std::string Bureaucrat::getName() const { return this->_name; }
+int Bureaucrat::getGrade() const { return this->_grade; }
+
+/* --------------------------------- Others --------------------------------- */
+void Bureaucrat::increment() {
+    if (this->_grade == 1)
+        throw Bureaucrat::GradeTooHighException();
+
+    this->_grade--;
+}
+void Bureaucrat::decrement() {
+    if (this->_grade == 150)
+        throw Bureaucrat::GradeTooLowException();
+
+    this->_grade++;
+}
+
+/* ------------------------------- Exceptions ------------------------------- */
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+    return ("grade too high");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+    return ("grade too low");
+}
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &obj) {
+    o << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+
+    return o;
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,166 @@
+#include "Bureaucrat.hpp"
+#include "colors.hpp"
+#include <iostream>
+
 int main() {
-    int i = 0;
+
+    std::cout << "Tests with (*) at the begining should print an exception"
+              << std::endl
+              << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 1) Using default constructer" << std::endl;
+    try {
+        Bureaucrat obj;
+    } catch (std::exception &e) {
+        std::cout << RED << "Exception: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "TEST 2) Instanciate with valid values" << std::endl;
+    try {
+        Bureaucrat obj("John", 10);
+    } catch (std::exception &e) {
+        std::cout << RED << "THIS MESSAGE SHOULD NOT PRINT: " << e.what()
+                  << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "TEST 3) Instanciate with highest grade valid" << std::endl;
+    try {
+        Bureaucrat obj("John", 1);
+    } catch (std::exception &e) {
+        std::cout << RED << "THIS MESSAGE SHOULD NOT PRINT: " << e.what()
+                  << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "TEST 4) Instanciate with lowest grade valid" << std::endl;
+    try {
+        Bureaucrat obj("John", 150);
+    } catch (std::exception &e) {
+        std::cout << RED << "THIS MESSAGE SHOULD NOT PRINT: " << e.what()
+                  << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 5) Instanciate with grade 0" << std::endl;
+    try {
+        Bureaucrat obj("John", 0);
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 6) Instanciate with grade 151" << std::endl;
+    try {
+        Bureaucrat obj("John", 151);
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "TEST 7) Instanciate with grade 1, decrement 2 times and "
+                 "increment 2 times"
+              << std::endl;
+    try {
+        Bureaucrat obj("John", 1);
+        obj.decrement();
+        obj.decrement();
+        obj.increment();
+        obj.increment();
+        std::cout << obj << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "TEST 8) Instanciate with grade 150, decrement and increment"
+              << std::endl;
+    try {
+        Bureaucrat obj("John", 150);
+        obj.increment();
+        obj.decrement();
+        std::cout << obj << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 9) Instanciate with grade 150 and decrement"
+              << std::endl;
+    try {
+        Bureaucrat obj("John", 150);
+        obj.decrement();
+        std::cout << obj << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 10) Instanciate with grade 1 and increment"
+              << std::endl;
+    try {
+        Bureaucrat obj("John", 1);
+        obj.increment();
+        std::cout << obj << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 11) Instanciate with grade 1, instanciate by copy and "
+                 "increment"
+              << std::endl;
+    try {
+        Bureaucrat one("John", 1);
+        Bureaucrat two(one);
+        two.increment();
+        std::cout << one << std::endl;
+        std::cout << two << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
+
+    /* ------------------------------------ */
+
+    std::cout << "* TEST 12) Instanciate with grade 1, assign and increment"
+              << std::endl;
+    try {
+        Bureaucrat one("John", 1);
+        Bureaucrat two("Jane", 20);
+        std::cout << two << std::endl;
+        two = one;
+        std::cout << two << std::endl;
+        two.increment();
+        std::cout << one << std::endl;
+        std::cout << two << std::endl;
+    } catch (std::exception &e) {
+        std::cout << RED << "Execption: " << e.what() << RESET << std::endl;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
